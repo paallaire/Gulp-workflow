19f614e4f11c5db4f2518a7330d95d9d0347c248
Pascal-André Allaire <pascalandre.allaire@gmail.com>
2018-03-04 22:53:14 -0500

    sticky

--- assets/scripts/utils/sticky.js
+++ assets/scripts/utils/sticky.js
@@ -19,29 +19,33 @@ export class Sticky {
 
             // working !!
             window.addEventListener("scroll", (e) => {
-              // this.scroll();
+             // this.scroll();
             });
 
             // why not working ?
-             window.addEventListener("scroll", this.scroll() );
+             window.addEventListener("scroll", this.scroll.bind(this) );
 
         }
     }
 
     scroll() {
+        console.log( this)
         let top = window.pageYOffset;
-
-        console.log(top + " vs " + this.offsetTop);
 
         if (top >= this.offsetTop) {
             this.el.classList.add('is-sticky');
         } else {
             this.el.classList.remove('is-sticky');
         }
+
     }
 
+    /* TO DO : HOW remove the listener ? */
     destroy() {
-        window.removeEventListener("scroll", this.scroll());
+        window.removeEventListener("scroll", this.scroll.bind(this) );
     }
 
 }
+
+
+// Thank for the answer but with this technique how I remove the "EventListerner" ?
