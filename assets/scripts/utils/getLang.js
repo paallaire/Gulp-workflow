79283a87eedee5fee1fa72cc519c816fe542fc10
paallaire <pascalandre.allaire@gmail.com>
2018-10-04 10:39:02 -0400

    Clean up

--- assets/scripts/utils/getLang.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-
-    let html = document.querySelector('html');
-
-    return html.getAttribute('lang') != null ? html.getAttribute('lang') : 'en';
-    
-}
--- assets/scripts/utils/parallax.js
+++
@@ -1,9 +0,0 @@
-import Rellax from 'rellax';
-
-export default function parallax() {
-    if (document.querySelectorAll('.rellax').length > 0) {
-        var rellax = new Rellax('.rellax', {
-            speed: -4
-        });
-    }
-}
