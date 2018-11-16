9d65c4a7ce1af14c4efd6317f8b8104bed42d82c
paallaire <pascalandre.allaire@gmail.com>
2018-11-16 17:04:15 -0500

    base grid

---
+++ assets/scripts/components/ShowGrid.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="show-grid">
+    <button class="show-grid__button" @click="toggle">{{ btnLabel }} Grid</button>
+
+    <transition name="show-grid">
+    <div class="show-grid__visual" v-if="isActive">
+
+      <div class="show-grid__container" :class="containerClass">
+        <div class="show-grid__row" :class="rowClass">
+          <div class="show-grid__column" :class="columnClass" v-for='(item, index) in columns' :key='index'></div>
+        </div>
+      </div>
+
+    </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { mapMutations } from "vuex";
+export default {
+  name: "showGrid",
+  props: {
+    columns: {
+      type: Number,
+      default: 12
+    },
+    containerClass: {
+      type: String,
+      default: "l-wrapper"
+    },
+    rowClass: {
+      type: String,
+      default: "row"
+    },
+    columnClass: {
+      type: String,
+      default: "col-small-1"
+    }
+  },
+  data() {
+    return {
+      isActive: false,
+      btnLabel: "Show"
+    };
+  },
+  computed: {
+    ...mapGetters(["env"])
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive;
+
+      if (this.isActive) {
+        this.btnLabel = "Hide";
+      } else {
+        this.btnLabel = "Show";
+      }
+      
+    }
+  },
+  mounted() {},
+  destroyed() {}
+};
+</script>
