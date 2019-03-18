a9cb013b2aba8f8e709b353a620193dcbcaf6f95
paallaire <pascalandre.allaire@gmail.com>
2019-03-18 11:09:18 -0400

    update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,40 +9,46 @@ import 'svgxuse';
 Helpers
 -------------------------------------------------------------------------------- */
 import bowser from 'bowser';
-import ShowGridInit from './utils/helper/showGrid';
-
-import formInit from './utils/form/main.js';
-import swiperInit from './utils/swiper';
-import rellaxInit from './utils/rellax';
-import aosInit from './utils/aos.js';
-import lazyInit from './utils/lazyload';
-
-/* --------------------------------------------------------------------------------
-Vue
--------------------------------------------------------------------------------- */
 import Vue from 'vue';
 import {
     mapGetters,
     mapMutations,
 } from 'vuex';
+
+import ShowGridInit from './utils/helper/showGrid';
+import formInit from './utils/form/main';
+import swiperInit from './utils/swiper';
+import rellaxInit from './utils/rellax';
+import aosInit from './utils/aos';
+import lazyInit from './utils/lazyload';
+
+/* --------------------------------------------------------------------------------
+Vue
+-------------------------------------------------------------------------------- */
 import store from './store';
+
+/* --------------------------------------------------------------------------------
+Vue - Components
+-------------------------------------------------------------------------------- */
+import modal from './vue-components/Modal.vue';
+import navCanvas from './vue-components/NavCanvas.vue';
 
 /* --------------------------------------------------------------------------------
 Vue - Directives
 -------------------------------------------------------------------------------- */
-let VueScrollTo = require('vue-scrollto');
+const VueScrollTo = require('vue-scrollto');
 
-let easings = {
-    'ease': [0.25, 0.1, 0.25, 1.0],
-    'linear': [0.00, 0.0, 1.00, 1.0],
+const easings = {
+    ease: [0.25, 0.1, 0.25, 1.0],
+    linear: [0.00, 0.0, 1.00, 1.0],
     'ease-in': [0.42, 0.0, 1.00, 1.0],
     'ease-out': [0.00, 0.0, 0.58, 1.0],
     'ease-in-out': [0.42, 0.0, 0.58, 1.0],
-    'material': [0.4, 0.0, 0.2, 1.0]
-}
+    material: [0.4, 0.0, 0.2, 1.0],
+};
 
 Vue.use(VueScrollTo, {
-    container: "body",
+    container: 'body',
     duration: 500,
     easing: [0.4, 0.0, 0.2, 1.0],
     offset: 0,
@@ -51,14 +57,8 @@ Vue.use(VueScrollTo, {
     onDone: false,
     onCancel: false,
     x: false,
-    y: true
-})
-
-/* --------------------------------------------------------------------------------
-Vue - Components
--------------------------------------------------------------------------------- */
-import modal from './vue-components/Modal.vue';
-import navCanvas from './vue-components/NavCanvas.vue';
+    y: true,
+});
 
 /* --------------------------------------------------------------------------------
 APP
@@ -70,7 +70,7 @@ const vm = new Vue({
     store,
     components: {
         navCanvas,
-        modal
+        modal,
     },
     data: {},
     computed: {
@@ -91,7 +91,6 @@ const vm = new Vue({
         ]),
     },
     mounted() {
-
         formInit(this.lang);
         swiperInit();
         lazyInit();
@@ -105,7 +104,6 @@ const vm = new Vue({
         if (!bowser.msie) {
             rellaxInit();
         }
-
     },
     destroyed() {},
 });
