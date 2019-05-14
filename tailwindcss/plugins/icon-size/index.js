628b404c9d4929836a17bcfb1b4633546ccf92f1
paallaire <pascalandre.allaire@gmail.com>
2019-05-13 21:55:37 -0400

    update tailwind

--- tailwindcss/plugins/icon-size/index.js
+++ tailwindcss/plugins/icon-size/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable func-names */
-module.exports = function (variants) {
-    return function ({ addUtilities }) {
-        addUtilities({
-            '.icon-size-sm': { 'font-size': '10px' },
-            '.icon-size-xs': { 'font-size': '20px' },
-            '.icon-size-md': { 'font-size': '30px' },
-            '.icon-size-lg': { 'font-size': '50px' },
-            '.icon-size-xl': { 'font-size': '60px' },
-        }, variants);
+const _ = require('../../../node_modules/lodash');
+
+module.exports = function ({ variants = ['responsive'] }) {
+    return function ({ e, addUtilities, config }) {
+        addUtilities([
+            ..._.map(config('theme.iconSize', {}), (value, key) => ({
+                [`.icon-size-${e(key)}`]: {
+                    'font-size': `${value}`,
+                },
+            })),
+        ], variants);
     };
 };
